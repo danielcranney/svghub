6f0a3f5bf80b88524dedfd0add132c5bdadc961e
djcranney <danieljamescranney@gmail.com>
2023-06-03 17:43:41 +0100

    :1 Google Analytics setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,34 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 function MyApp({ Component, pageProps }) {
   // const getLayout = Component.getLayout || ((page) => page);
 
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="ganalytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
